b6f06d1c9a4af970120129d183206c0fb824f6f2
socsieng <socsieng@gmail.com>
2018-04-05 21:35:23 +1000

    Remove websocket ping/pong
    Fixes #11

--- cmd/rest-api/routes.go
+++ cmd/rest-api/routes.go
@@ -5,24 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"gopkg.in/mgo.v2/bson"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
-)
-
-const (
-	// Time allowed to write the file to the client.
-	writeWait = 10 * time.Second
-
-	// Time allowed to read the next pong message from the client.
-	pongWait = 60 * time.Second
-
-	// Send pings to client with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
 )
 
 var (
@@ -105,38 +93,5 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 		}{
 			msg.TopicPartition.Topic,
 		})
-	}
-
-	go writer(ws)
-	reader(ws)
-}
-
-func writer(ws *websocket.Conn) {
-	pingTicker := time.NewTicker(pingPeriod)
-	defer func() {
-		pingTicker.Stop()
-		ws.Close()
-	}()
-	for {
-		select {
-		case <-pingTicker.C:
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
-				return
-			}
-		}
-	}
-}
-
-func reader(ws *websocket.Conn) {
-	defer ws.Close()
-	ws.SetReadLimit(512)
-	ws.SetReadDeadline(time.Now().Add(pongWait))
-	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
-	for {
-		_, _, err := ws.ReadMessage()
-		if err != nil {
-			break
-		}
 	}
 }
